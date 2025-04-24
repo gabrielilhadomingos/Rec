d28c4cbcd1547e577fd8614dae7783fc61f77534
GABRIEL DOMINGOS ILHA <gabriel_ilha@estudante.sesisenai.org.br>
2025-04-24 07:48:46 -0300

    Ataques para cada classe herdada do personagem

--- Rec.js
+++ Rec.js
@@ -1,5 +1,5 @@
 class Personagem{
-    constructor(){
+    constructor(vida, força, mana){
         this.vida = vida,
         this.força = força,
         this.mana - mana
@@ -8,5 +8,22 @@ class Personagem{
 }
 
 class Guerreiro extends Personagem{
-    
+    constructor(vida, força, ataque, espada){
+        this.ataque = ataque,
+        this.espada = espada
+    }
+}
+
+class Mago extends Personagem{
+    constructor(vida, mana, dm, cajado){
+        this.dm = dm,
+        this.cajado = cajado
+    }
+}
+
+class Arqueiro extends Personagem{
+    constructor(vida, força, tiro, arco){
+        this.tiro = tiro,
+        this.arco = arco
+    }
 }
